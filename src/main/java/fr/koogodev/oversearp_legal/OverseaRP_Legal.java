4351a1f13c159b574641dc41425a252288b82614
koog0 <168228440+koog0@users.noreply.github.com>
2024-08-23 21:26:25 +0200

    Update OverseaRP_Legal.java

--- src/main/java/fr/koogodev/oversearp_legal/OverseaRP_Legal.java
+++ src/main/java/fr/koogodev/oversearp_legal/OverseaRP_Legal.java
@@ -20,7 +20,6 @@ public final class OverseaRP_Legal extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 
     public HashMap<UUID, ArmorStand> gethashMap(){
